121e0dcdf173bf6dfd5785b17c25364610ce4f12
Henry Leon <helg18@gmail.com>
2025-03-02 23:32:40 -0400

    Adding CARDANO

---
+++ ada.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# define currency
+currency=ada
+
+# build url
+endpoint=https://api.coincap.io/v2/assets/$currency
+
+# make request and get price
+respuesta=$(curl -s $endpoint | jq -r '.data.priceUsd')
+
+# build message to send notify
+precio="Precio : $respuesta USD" # Usar comillas dobles para la expansión
+
+# sending notify to screen
+/usr/bin/notify-send "$currency" "$precio"
