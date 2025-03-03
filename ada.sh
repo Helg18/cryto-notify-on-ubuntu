ac699b855ba3a3ec56bf92d85d2c1f687c9724b7
Henry Leon <helg18@gmail.com>
2025-03-03 03:11:26 -0400

    Refactor
    - Add path to dbus
    - Validate if exist dbus socket
    -  Error message when dbus failed

--- ada.sh
+++ ada.sh
@@ -1,16 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
-# define currency
-currency=cardano
+# Obtener el usuario actual
+USER=$(who | grep "(:0)" | awk '{print $1}')
 
-# build url
-endpoint=https://api.coincap.io/v2/assets/$currency
+# Obtener el ID del usuario
+USER_ID=$(id -u "$USER")
 
-# make request and get price
-respuesta=$(curl -s $endpoint | jq -r '.data.priceUsd')
+# Obtener el DBUS_SESSION_BUS_ADDRESS desde el archivo de sesión
+DBUS_PATH="/run/user/$USER_ID/bus"
 
-# build message to send notify
-precio="Precio : $respuesta USD" # Usar comillas dobles para la expansión
+# Verifica si el socket de D-Bus existe
+if [ -S "$DBUS_PATH" ]; then
 
-# sending notify to screen
-/usr/bin/notify-send "$currency" "$precio"
+    # Exportamos las variables del bus necesarios para el notify-send
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=$DBUS_PATH"
+    export DISPLAY=:0
+
+    # Definimos la cripto
+    currency=cardano
+
+    # Construimos la url
+    endpoint=https://api.coincap.io/v2/assets/$currency
+
+    # Ejecutar la peticion y obtener el precio
+    respuesta=$(curl -s $endpoint | jq -r '.data.priceUsd')
+
+    # Construimos el cuerpo del notify-send
+    precio="Precio : $respuesta USD" # Usar comillas dobles para la expansión
+
+    # Enviamos la notificacion al usuario
+    /usr/bin/notify-send "$currency" "$precio"
+
+# Sino enviamos un mensaje de error
+else
+
+    # Mensaje de error
+    echo "Error: No se pudo encontrar el bus D-Bus para el usuario $USER."
+fi
